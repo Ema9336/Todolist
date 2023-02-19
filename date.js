e0994f3d94f7b8f0343c00686b345891d98c3d60
Ema9336 <122706691+Ema9336@users.noreply.github.com>
2023-02-20 00:13:11 +0100

    Add files via upload

---
+++ date.js
@@ -0,0 +1,31 @@
+
+exports.getDate = function () {
+
+  const today = new Date();
+ 
+  const options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long"
+  };
+
+  return today.toLocaleDateString("en-US", options);
+  
+  
+}
+
+module.exports.getDay = getDay;
+
+function getDay() {
+
+  const today = new Date();
+ 
+  const options = {
+    weekday: "long"
+    
+  };
+
+  let day = today.toLocaleDateString("en-US", options);
+  
+  return day;
+}
